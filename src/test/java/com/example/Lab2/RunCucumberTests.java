bccd35f7017c76a2a315cf6aa1d21a4a804cbd99
bagch <swetabagchi2014@gmail.com>
2024-05-28 02:32:24 +0200

    Changing line 128 into FALSE Generating cucumber report Adding headless Adding glue

--- src/test/java/com/example/Lab2/RunCucumberTests.java
+++ src/test/java/com/example/Lab2/RunCucumberTests.java
@@ -7,7 +7,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/com/example/Lab2/resources/features",
         plugin = {"pretty" ,"html:target/cucumber-report.html"},
-        glue = {"helpers","src/test/java/com/example/Lab2/StepDefinition.java"})
+        glue = {"helpers","src/test/java/com/example/Lab2/StepDefinition.java"} ,
+        dryRun = true,
+        monochrome = true)
 
 public class RunCucumberTests {
 
