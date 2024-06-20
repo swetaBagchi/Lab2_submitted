0011c8106d584b82ce5568a279818c2b6727b4e6
bagch <swetabagchi2014@gmail.com>
2024-06-21 00:27:57 +0200

    Merge remote-tracking branch 'origin/master'

--- src/test/java/com/example/Lab2/RunCucumberTest.java
+++ src/test/java/com/example/Lab2/RunCucumberTest.java
@@ -5,7 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/com/example/Lab2/resources/features")
+@CucumberOptions(features = "src/test/java/com/example/Lab2/resources/features",
+                  plugin = {"pretty" ,"html:target/Cucumber-Reports.html"}
+                )
 
 public class RunCucumberTest {
 
